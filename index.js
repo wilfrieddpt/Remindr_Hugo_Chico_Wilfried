4b4174122fe9c484f99cf00dfbc0ec53139caccd
wilfrieddpt <120278338+wilfrieddpt@users.noreply.github.com>
2023-12-01 12:11:16 +0100

    ajout de tout les fichiers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
@@ -25,4 +26,36 @@ app.get('/accueil', (req, res) => {
 const port = 3001;
 app.listen(port, () => {
     console.log(`Le serveur écoute sur le port ${port}`);
+=======
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+// Configuration de Handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Définir une route exemple
+app.get('/', (req, res) => {
+    res.render('accueil'); // Assure-toi d'avoir un fichier accueil.handlebars dans ton dossier "views"
+});
+
+// Définir une route exemple
+app.get('/signup', (req, res) => {
+  res.render('signup', { message: 'Page d inscription' });
+});
+
+app.post('/signup', (req, res) => {
+  res.render('signup', { message: 'Page d inscription' });
+});
+
+// Démarrer le serveur
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Le serveur écoute sur le port ${port}`);
+>>>>>>> Stashed changes
 });
