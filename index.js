8d9f5a836808392da416a02ab0b5d201c4dd0c8f
HugoFaveyrial <hugo.faveyrial@etu.uca.fr>
2023-12-14 16:32:53 +0100

    Le site est plutot fonctionnel et il y a une barre en haut

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
+//Importer express
 const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
-
-
-
-// Importe Prisma
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-
-
 const app = express();
 
-
-
-// Configuration de Handlebars
+//Importer Handlebars
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));    
+//Cette ligne sert à éviter de préciser dans les routes que les fichiers sont dans le dossier views
+app.set('views', path.join(__dirname, 'views')); 
 
 
+// Importer Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
+//configuration de la session
 app.use(session({
   secret: 'secret_key',
   resave: false,
@@ -31,96 +26,21 @@ app.use(session({
       maxAge: 3600000000 // Durée de la session en millisecondes (1 heure dans cet exemple)
   }
 }));
-
-
-
-
+   
 // Middleware pour parser les données du formulaire
 app.use(express.urlencoded({ extended: true }));
 
+//****************************************************************
 
-
-// Définir une route exemple
+// Création des routes
 app.get('/', (req, res) => {
-  res.render('accueil'); // Assure-toi d'avoir un fichier accueil.handlebars dans ton dossier "views"
+  res.render('accueil', {layout:'main.handlebars'});
 });
 
-
-
-// Route pour afficher le formulaire d'inscription
-app.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-
-
-// Route pour traiter les données du formulaire d'inscription
-app.post('/signup', async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    // Utilise Prisma pour vérifier si l'e-mail est déjà utilisé
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (existingUser) {
-      // L'e-mail est déjà utilisé, affiche un message d'erreur
-      return res.render('signup', { error: 'Cette adresse e-mail est déjà associée à un compte.' });
-    }
-
-    // L'e-mail n'est pas déjà utilisé, procède à l'inscription
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password,  // Assure-toi de hacher et de saler le mot de passe avant de le stocker dans la base de données
-      },
-    });
-
-    console.log('Utilisateur enregistré avec succès:', user);
-
-    // Redirige l'utilisateur vers une page de confirmation ou une autre page appropriée
-    res.redirect('/confirmation');
-  } catch (error) {
-    console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
-    res.render('signup', { error: 'Une erreur s\'est produite lors de l\'inscription.' });
-  }
-});
-
-
-
-// Route pour afficher la page de confirmation
-app.get('/confirmation', (req, res) => {
-  res.render('confirmation');
-});
-
-app.get('/dashboard', (req, res) => {
-    const fakeTasks = [
-        {
-            title: 'Tache 1',
-            color: 'bleu',
-            description: 'Faire la tache 1',
-            Deadline: '24/08/2002',
-            validated: false
-        }
-    ];
-    const username = { username: req.session.user.username };
-    res.render('Tableau_De_Bord', { tasks: fakeTasks }, { username });
-});
-
-
-
-// Route pour afficher le formulaire de connexion
 app.get('/login', (req, res) => {
   res.render('login');
 });
 
-
-
-// Route pour traiter les données du formulaire de connexion
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -140,8 +60,8 @@ app.post('/login', async (req, res) => {
     // Vérifie le mot de passe (en comparant en texte brut)
     if (password === user.password) {
       // Authentification réussie, enregistre l'utilisateur dans la session
-      req.session.user = { id: user.id, username: user.username };
-      res.redirect('/dashboard'); // Redirige vers la page du tableau de bord ou toute autre page
+      req.session.user = { username: user.username };
+      res.redirect('/dashboard');
     } else {
       // Mot de passe incorrect, redirige vers la page de login
       res.render('login', { error: 'Nom d\'utilisateur ou mot de passe incorrect' });
@@ -152,22 +72,48 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
+app.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // Recherche l'utilisateur dans la base de données
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (existingUser) {
+      // L'e-mail est déjà utilisé, affiche un message d'erreur
+      return res.render('signup', { error: 'Cette adresse e-mail est déjà associée à un compte.' });
+    }
 
-app.get('/dashboard', (req, res) => {
-  // Vérifie si l'utilisateur est connecté
-  if (req.session.user) {
-    // Utilisateur connecté, rend la page du tableau de bord
-    res.render('dashboard', { username: req.session.user.username });
-  } else {
-    // Utilisateur non connecté, redirige vers la page de login
-    res.redirect('/login');
+    // L'e-mail n'est pas déjà utilisé, on procède à l'inscription
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,E
+      },
+    });
+
+    console.log('Utilisateur enregistré avec succès:', user);
+
+    // Redirige l'utilisateur vers une page de confirmation ou une autre page appropriée
+    res.redirect('/confirmation');
+  } catch (error) {
+    console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
+    res.render('signup', { error: 'Une erreur s\'est produite lors de l\'inscription.' });
   }
 });
 
+app.get('/confirmation', (req, res) => {
+  res.render('confirmation');
+});
 
-
-app.post('/logout', (req, res) => {
+app.get('/logout', (req, res) => {
   // Déconnecte l'utilisateur en détruisant la session
   req.session.destroy((err) => {
       if (err) {
@@ -180,7 +126,42 @@ app.post('/logout', (req, res) => {
   });
 });
 
+app.get('/dashboard', async(req, res) => {
+  // Vérifie si l'utilisateur est connecté
+  if (req.session.user) {
+    const myUser = { username: req.session.user.username };
+    const fakeTasks = await prisma.Task.findMany({
+      where: {
+          group: {
+              users: {
+                  some: {
+                      username: myUser.username
+                  }
+              }
+          }
+      },
+      select: {
+          group: true,
+          publisher: true,
+          dateSubmit: true,
+          color: true,
+          name: true,
+          description: true,
+          validated: true
+      },
+      orderBy: {
+          dateSubmit: 'asc'
+      }
+    });
+    res.render('dashboard', { tasks: fakeTasks, username: myUser.username });
+  } else {
+    // Utilisateur non connecté, redirige vers la page de login
+    res.redirect('/login');
+  }
+});
+  
 
+//****************************************************************
 
 // Démarrer le serveur
 const port = 3001;
